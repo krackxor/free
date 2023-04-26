efeac01ea1f37ff04d97580cd348d53647b7bfbf
krackxor <77487814+krackxor@users.noreply.github.com>
2023-04-27 04:24:36 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/krackxor/vip.git /vip
+  git clone https://github.com/krackxor/free.git /free
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /vip
+  git clone $UPSTREAM_REPO /free
 fi
-cd /vip
+cd /free
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
