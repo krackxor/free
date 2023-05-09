1e77a5176a77700a49afc21bc4af2c444302bbed
krackxor <77487814+krackxor@users.noreply.github.com>
2023-05-09 20:53:02 +0700

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://tmid:tmid@cluster0.ej5ao.mongodb.net/?retryWrites=true&w=majority')
+DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://tmid:tmid@cluster0.d4epj5z.mongodb.net/?retryWrites=true&w=majority')
 DATABASE_NAME = environ.get('DATABASE_NAME', 'TMID')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files_free')
 
