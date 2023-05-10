de59c593faba1007d2404de3f8831aeca1e04a97
krackxor <77487814+krackxor@users.noreply.github.com>
2023-05-10 11:13:20 +0700

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://tmid:tmid@cluster0.d4epj5z.mongodb.net/?retryWrites=true&w=majority')
-DATABASE_NAME = environ.get('DATABASE_NAME', 'TMID')
+DATABASE_NAME = environ.get('DATABASE_NAME', 'TMIDFilter')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Other
