cc1b7ce0195aa7c9915b59d1c7c940d913db4b92
krackxor <77487814+krackxor@users.noreply.github.com>
2023-05-11 11:19:00 +0700

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), False
 
 #LazyRenamer Configs
 FLOOD = int(environ.get("FLOOD","10"))
-LAZY_MODE = bool(environ.get("LAZY_MODE","False"))
+LAZY_MODE = bool(environ.get("LAZY_MODE"))
 
 #Add user id of the user in this field those who you want to be Authentic user for file renaming features
 lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '1242025652').split()]
